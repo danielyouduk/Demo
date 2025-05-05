b4ca6ebde64d0ad595f49696f8847db1139cfe66
danielyouduk <daniel.youd@icloud.com>
2025-05-05 10:39:51 +0100

    WIP

--- src/Services/SearchService/API/SearchService.Api/Models/Driver.cs
+++ src/Services/SearchService/API/SearchService.Api/Models/Driver.cs
@@ -9,6 +9,7 @@ public class DriverEntity : ITableEntity
     public string PartitionKey { get; set; }
     public string RowKey { get; set; }
     public DateTimeOffset? Timestamp { get; set; }
+    
     public ETag ETag { get; set; }
 
     public string Category { get; set; }
