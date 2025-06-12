53cf2676b81c599f9fd9e0a05f2e2a7f39fae2df
danielyouduk <daniel.youd@icloud.com>
2025-06-12 08:34:04 +0100

    Add Navbar and Sidebar components with basic structure
    Introduced `NavbarComponent` and `SidebarComponent` as standalone components with basic templates, styles, and tests. Updated `HomeComponent` and `DashboardHomeComponent` to include the new components, improving layout modularity.

--- src/Apps/my-demo-account/src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
+++ src/Apps/my-demo-account/src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from '../../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard-pages',
-  imports: [],
+  imports: [
+    SidebarComponent
+  ],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.css'
 })
